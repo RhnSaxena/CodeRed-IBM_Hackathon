ab435547dbbcd0e0ffa90b27a26f3c895611f30d
Siddharth <sidkv.skv@gmail.com>
2019-03-03 03:58:50 +0530

    Addition of whole code

---
+++ ibm/index.js
@@ -0,0 +1,138 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var multer  = require('multer');
+var fs  = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+var text = "Rep: This is Himanshu. How can I help you? Caller: This is Alex. I want to change my plan to corporate plan Rep: Sure, I can help you. Do you want to change the plan for the number from which you are calling now? Caller: yes Rep: For verification purpose may I know your date of birth and email id Caller: My data of birth is 10-Aug-1979 and my email id is alex@gmail.com Rep: Which plan do you want to migrate to Caller: Plan 450 unlimited Rep: Can I have your company name and date of joining Caller: I work for IBM and doj 01-Feb-99 Rep: Ok.. I have taken your request to migrate plan to 450 unlimited. You will get an update in 3 hours. Is there anything else that I can help you with Caller: No Rep: Thanks for calling Vodaphone. Have a good day. Caller: you too',"
+var output = {};
+var answer="";
+
+function analyze(){
+
+  var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+  var naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    'version': '2018-11-16',
+    'iam_apikey': '2Inwv2Q4telyv6PIwl-8O2Lc0y4bn56HamlPZyzOX6Xq',
+    'url': "https://gateway-fra.watsonplatform.net/natural-language-understanding/api",
+  });
+
+  var parameters = {
+    'text': text,
+    'features': {
+      "entities": {
+          "model": "aecf206d-caba-4c17-99ec-fc47bd115e38"
+      },
+      "relations": {
+        "model": "aecf206d-caba-4c17-99ec-fc47bd115e38"
+      },
+      'keywords': {
+        'emotion': true,
+        'sentiment': true,
+        'limit': 2
+      }
+    }
+  }
+
+  naturalLanguageUnderstanding.analyze(parameters, function(err, response) {
+    if (err){
+      console.log('error:', err);
+    }
+    else{
+
+      var g;
+      var h=[];
+      var t=false;
+      output =response;
+      console.log(output);
+      g = text.split(" ");
+      console.log(g);
+      g.forEach((y)=>{
+        t=false;
+        output.entities.forEach((x)=>{
+           if(y.localeCompare(x.text)==0){
+            console.log("hey");
+            h.push("***********");
+            t=true;
+          }else{
+            console.log(y+" "+x.text);
+          }
+        });
+        if(t==false){
+            h.push(y);
+        }
+      });
+      answer=h.join(" ");
+    }
+  });
+
+}
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+app.get("/loading",function(req,res){
+  res.render("loading");
+})
+
+app.get('/redact', function (req, res) {
+  res.render("redact",{out:answer});
+});
+
+app.post('/analyze', function (req, res) {
+  text = req.body.tex;
+  analyze();
+  res.redirect("/loading");
+});
+
+
+
+
+
+
+
+
+var filenam="";
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        var dir = './uploads';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        callback(null, dir);
+    },
+    filename: function (req, file, callback) {
+      filenam="uploads/"+file.originalname;
+        callback(null, file.originalname);
+    }
+});
+var upload = multer({storage: storage}).array('files', 12);
+
+app.post('/upload', function (req, res, next) {
+
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong:(");
+        }else{
+            var fs = require("fs");
+            fs.readFile(filenam, function(err, buf) {
+              text = buf;
+              console.log(buf);
+            });
+
+          res.end("Upload completed."+filenam+" "+text);
+        }
+    });
+
+})
+
+app.listen(3000,function(){
+console.log("Server On");
+});
